47022bde45a36489ec8a0a5b5c32aea95f2fcb2e
joao <jvictorcs35@gmail.com>
2026-07-06 20:19:22 -0300

    oi

---
+++ repository/index.js
@@ -0,0 +1,14 @@
+//um array são vários espaços na mémoria
+
+const bancoDeDados =[]
+
+
+//[]
+bancoDeDados.push("joao")
+// [ "joao" ]
+bancoDeDados.push("pedro")
+// [ ""joao" , "pedro"]
+bancoDeDados.push("alberto")
+
+
+export default bancoDeDados
