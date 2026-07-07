3fb886c0907587abe66a5865dc2f98d2aafb248b
joao <jvictorcs35@gmail.com>
2026-07-06 21:39:35 -0300

    commit guloso

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
 import express from 'express'
-import bancoDeDados from './repository'
-
-//server ou app
+import bancoDeDados from './repository/index.js'
 
 const app = express()
-//path params - router
-//app.get("/api/v1/somar", (req, res) => {
-    // query params 
-//    const num1 = Number(req.query.num1)
-//    const num2 = Number(req.query.num2)
-//    const resultado = num1 + num2
-
-//    res.send({ resultado })
-//}) 
-
-// aqui em diante
-
-// metodo GET da função de subtrair usando QueryParam
-// Com numero1 e numero2 ou num1 e num2
-
-app.get("/api/pessoa/:id" , (req, res) => {
-
-    bancoDeDados.find()
-
-
-    res.send({ message: "Hello World" })
+//buscar
+app.get("/api/pessoa/:id", (req, res) => {
+    const id = req.params.id
+    const pessoa = bancoDeDados.find(it => it.id == id)
+    if (!pessoa) {
+        res.send({ message: "Pessoa não encontrada" })
+        return
+    }
+    res.send({ pessoa })
 })
-
-
-app.get("/api/pessoa" , (req, res) => {
-    const id = req.query.id
-    const name = req.query.name
-
-    bancoDeDados.push
-
-    res.send({ message: "pessoa criada com sucesso!!!" })
+//criar
+app.get("/api/pessoa", (req, res) => {
+    const { id, name } = req.query
+    if(!id || !name) {
+        res.send({ message: "Favor informar id e name" })
+        return 
+    }
+    bancoDeDados.push({ id, name })
+    res.send({ message: "Pessoa criada com sucesso" })
 })
 
 app.listen(3000, () => {
--- repository/index.js
+++ repository/index.js
@@ -1,14 +1,3 @@
-//um array são vários espaços na mémoria
-
-const bancoDeDados =[]
-
-
-//[]
-bancoDeDados.push("joao")
-// [ "joao" ]
-bancoDeDados.push("pedro")
-// [ ""joao" , "pedro"]
-bancoDeDados.push("alberto")
-
+const bancoDeDados = []
 
 export default bancoDeDados
