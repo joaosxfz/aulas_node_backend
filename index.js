5fceab43bcf8531daefc33b1aa274f8b1ce95fd6
joao <jvictorcs35@gmail.com>
2026-07-13 22:15:05 -0300

    oi

--- index.js
+++ index.js
@@ -2,25 +2,13 @@ import express from 'express'
 import bancoDeDados from './repository/index.js'
 
 const app = express()
+app.use(express.json())
+
 //buscar
-app.get("/api/pessoa/:id", (req, res) => {
-    const id = req.params.id
-    const pessoa = bancoDeDados.find(it => it.id == id)
-    if (!pessoa) {
-        res.send({ message: "Pessoa não encontrada" })
-        return
-    }
-    res.send({ pessoa })
-})
-//criar
-app.get("/api/pessoa", (req, res) => {
-    const { id, name } = req.query
-    if(!id || !name) {
-        res.send({ message: "Favor informar id e name" })
-        return 
-    }
-    bancoDeDados.push({ id, name })
-    res.send({ message: "Pessoa criada com sucesso" })
+app.get("/api/v1/produto", (req, res) => {
+    const { produto, preco } = req.query
+
+    res.status(200).send({ message: produto + preco })
 })
 
 app.listen(3000, () => {
