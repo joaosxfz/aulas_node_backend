f8a54da83b3b77f4be30eebaf7f9296c0f59e8fd
joao <jvictorcs35@gmail.com>
2026-07-16 19:41:19 -0300

    4-aula-router

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 import express from 'express'
+import cachorro from './router/cachorro.js'
+
 const app = express()
 
 app.use(express.json())
 
+app.use('/api/v1/cachorro', cachorro )
 
 //somar
-app.post("/api/v1/somar", (req, res) => {
-    const { num1, num2 } = req.body
-    const resultado = Number(num1) + Number(num2)
-    //validações quanto ao número.... Renan apagou o código 😢
-    res.status(200).send({ message: resultado })
-})
+// app.post("/api/v1/somar", (req, res) => {
+//     const { num1, num2 } = req.body
+//     const resultado = Number(num1) + Number(num2)
+//     //validações quanto ao número.... Renan apagou o código 
+//     res.status(200).send({ message: resultado })
+// })
 
 
-//subtrair
-app.post("/api/v1/subtrair", (req, res) => {
-    const { num1, num2 } = req.post
-    const resultado = Number(num1) - Number(num2)
+// subtrair
+// app.post("/api/v1/subtrair", (req, res) => {
+//     const { num1, num2 } = req.post
+//     const resultado = Number(num1) - Number(num2)
 
-    res.status(200).send({ message: resultado })
-})
+//     res.status(200).send({ message: resultado })
+// })
 
-//multiplicar
-app.get("/api/v1/multiplicar", (req, res) => {
-    const { num1, num2 } = req.query
-    const resultado = Number(num1) * Number(num2)
+// multiplicar
+// app.get("/api/v1/multiplicar", (req, res) => {
+//     const { num1, num2 } = req.query
+//     const resultado = Number(num1) * Number(num2)
 
-    res.status(200).send({ message: resultado })
-})
+//     res.status(200).send({ message: resultado })
+// })
 
-//dividir
-app.get("/api/v1/dividir", (req, res) => {
-    const { num1, num2 } = req.query
-    const resultado = Number(num1) / Number(num2)
+// dividir
+// app.get("/api/v1/dividir", (req, res) => {
+//     const { num1, num2 } = req.query
+//     const resultado = Number(num1) / Number(num2)
 
-    res.status(200).send({ message: resultado })
-})
+//     res.status(200).send({ message: resultado })
+// })
 
 
 
