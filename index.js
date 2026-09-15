fed45bbe42dad2de758ef88f076c9b0917d11126
joaosxfz <jvictorcs35@gmail.com>
2026-09-14 22:16:52 -0300

    ok

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import carro from './router/carro.js'
 import database from './config/database.js'
+import usuario from './router/usuario.js'
+
 
 const app = express()
 app.use(express.json())
 
 app.use("/api/v1/carro", carro)
+app.use("/api/v1/usuario", usuario)
 
 database.db
     .sync({ force: false })
