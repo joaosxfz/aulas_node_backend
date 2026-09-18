4e352541cfbb2cca1eba191a6678c817b4e77c40
joaosxfz <jvictorcs35@gmail.com>
2026-09-18 19:06:33 -0300

    sabor

--- controller/usuario.js
+++ controller/usuario.js
@@ -1,101 +1,93 @@
-import ServiceUsuario from '../service/usuario.js';
+import ServiceUsuario from '../service/usuario.js'
 
 class ControllerUsuario {
-
-    // Recebimento e a Saída das informações
-
-    async Buscar(_, res) {
+    // Recebimento e a Saida das info
+    async Buscar(req, res) {  
         try {
-            const usuarios = await ServiceUsuario.Buscar();
-
-            res.status(200).send({
-                mensagem: usuarios
-            });
+            console.log(req.session)
+            const usuarios = await ServiceUsuario.Buscar()
+            res.status(200).send({ mensagem: usuarios })
         } catch (error) {
             res.status(500).send({
                 mensagem: error.message
-            });
+            })
         }
     }
 
     async Detalhe(req, res) {
         try {
-            const id = req.params.id;
+            const id = req.params.id
 
-            const usuario = await ServiceUsuario.Detalhe(id);
+            const usuario = await ServiceUsuario.Detalhe(id)
 
-            res.status(200).send({
-                mensagem: usuario
-            });
+            res.status(200).send({ mensagem: usuario })
         } catch (error) {
             res.status(500).send({
                 mensagem: error.message
-            });
+            })
         }
     }
 
     async Criar(req, res) {
         try {
-            const { email, senha } = req.body;
+            const { email, senha } = req.body
 
-            await ServiceUsuario.Criar(email, senha);
-
-            res.status(201).send({
-                mensagem: "Cadastrado com sucesso"
-            });
+            await ServiceUsuario.Criar(email, senha)
+            
+            res.status(201).send({ mensagem: "Cadastrado com sucesso" })
         } catch (error) {
             res.status(500).send({
                 mensagem: error.message
-            });
+            })
         }
     }
 
     async Alterar(req, res) {
         try {
-            const { email, senha } = req.body;
-            const id = req.params.id;
+            const { email, senha } = req.body
+            const id = req.params.id
 
-            await ServiceUsuario.Alterar(id, email, senha);
-
-            res.status(200).send({
-                mensagem: "Alterado com sucesso"
-            });
+            await ServiceUsuario.Alterar(id, email, senha)
+            
+            res.status(201).send({ mensagem: "Cadastrado com sucesso" })
         } catch (error) {
             res.status(500).send({
                 mensagem: error.message
-            });
+            })
         }
     }
 
     async Deletar(req, res) {
         try {
-            const identificador = req.params.id;
+            const identificador = req.params.id
 
-            await ServiceUsuario.Deletar(identificador);
+            await ServiceUsuario.Deletar(identificador)
 
-            res.status(204).send();
+            res.status(204).send({ mensagem: "Deletado" })
         } catch (error) {
+            
             res.status(500).send({
                 mensagem: error.message
-            });
+            })
         }
     }
 
     async Login(req, res) {
         try {
-            const { email, senha } = req.body;
+            const { email, senha } = req.body
 
-            const token = await ServiceUsuario.Login(email, senha);
+            const token = await ServiceUsuario.Login(email, senha)
 
             res.status(200).send({
                 token
-            });
+            })
         } catch (error) {
+            
             res.status(500).send({
                 mensagem: error.message
-            });
+            })
         }
     }
 }
 
-export default new ControllerUsuario();
+export default new ControllerUsuario()
--- middleware/auth.js
+++ middleware/auth.js
@@ -8,17 +8,19 @@ export default async function authMiddleware(req, res, next) {
     try {
         //verificar se o JWT é valido
         const token = req.headers['authorization']
+
+        console.log(token)
+
         if (!token) {
             throw new Error()
         }
-        console.log(token)
 
         const decoded = jwt.verify(token, segredo)
-        console.log(decoded)
-        next()
 
-    }
-    catch (error) {
+        req.session = decoded
+
+        next()
+    } catch (error) {
         res.status(403).send({
             message: "Usuario ou senha inválido"
         })
